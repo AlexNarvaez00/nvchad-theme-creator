fa13a479c95c117c309c5a2280f7cc45bea767d8
AlexisNarvaez00 <zaizealexis@gmail.com>
2024-02-01 18:41:39 -0600

    ft: serve added and imaga background changes

--- src/page/Index/Page.tsx
+++ src/page/Index/Page.tsx
@@ -4,7 +4,7 @@ import { IconAlertTriangleFilled } from "@tabler/icons-react";
 
 export default function Page() {
   return (
-    <section className="bg-[url(bg.jpg)] w-full min-h-screen bg-cover flex flex-col items-center pt-24">
+    <section className="bg-[url(/bg.jpg)] w-full min-h-screen bg-cover flex flex-col items-center pt-24">
       <h1 className="text-4xl max-w-[600px] text-center mb-12 font-bold text-[#cdd6f4]">
         Generador de temas para <span className="text-[#468bf7]">NvChad</span> del Editor Neovim.
       </h1>
@@ -12,7 +12,7 @@ export default function Page() {
         <div className="bg-[#313244] p-5 rounded-xl">
           <p>
             <IconAlertTriangleFilled size={"16px"} className="text-[#f9e2af] inline-block mr-2" />Hay algunas cosas que aun no estan pulidas, asi que ten cuidado, el
-            tema podria no funcionar al 100%
+            tema podria no funcionar al 100%, puede hacer una <a href="https://github.com/AlexNarvaez00/nvchad-theme-creator" target="_blank" className="underline">pull request</a>.
           </p>
         </div>
         <Link to={`/create`} className="py-3 px-4 font-bold mx-auto rounded-xl text-center text-white bg-[#11111b] border-2 border-white" >Ir a la applicación 🚀</Link>
